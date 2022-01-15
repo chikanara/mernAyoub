3abc4cc8c5092326dfa1478d940861ed7019e917
chikanara <usertestabc35@gmail.com>
2022-01-15 16:37:44 +0100

    backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const configDB = require("./config/configDB");
+require('dotenv').config({path:"./config/.env"})
+const authRouter = require("./routes/authRouter")
+const app = express();
+configDB()
+app.use(express.json())
+app.use("/api",authRouter)
+
+
+const PORT = 5000;
+app.listen(PORT, (err) =>
+  err ? console.error(err) : console.log("Server is running on port", PORT)
+);
